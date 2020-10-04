36c9a83dff889df4e133206fd6605907930a52f6
John Blank <veloblank@gmail.com>
2020-10-04 18:29:49 -0400

    Create and stylize slider with border.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
 window.onload = addListeners();
 
 function addListeners() {
+  addTableListeners();
+  addSliderListeners();
+}
+
+
+function addTableListeners() {
   let tables = document.getElementsByClassName("grid");
   let slider = document.getElementById("range-slider");
   for (let table of tables) {
     table.addEventListener("mouseenter", (e) => {
-      table.addEventListener("click", () => {
-        let rangeValue = (parseInt(e.target.getAttribute("data-range")))
-        slider.innerHTML = rangeValue + "%";
-      })
+      let rangeValue = (parseInt(e.target.getAttribute("data-range")))
+      slider.innerHTML = `<span id="range-value">${rangeValue}%</span>`;
     })
   }
+}
+
+
+function addSliderListeners() {
+  let slider = document.getElementById("range-slider");
+  slider.addEventListener("mouseenter", (e) => {
+    console.log(e.target)
+  })
+}
+
+function highlightRange() {
+
 }
