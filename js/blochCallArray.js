6a62603bbc3c99980b1fcd310d6f64deaf9505db
John Blank <veloblank@gmail.com>
2020-10-05 12:53:48 -0400

    Separate js files for ranges

---
+++ js/blochCallArray.js
@@ -0,0 +1,509 @@
+const BLOCH_CALL = [
+  {
+    chs1: "cell_AA"
+  },
+  {
+    chs2: "cell_KK"
+  },
+  {
+    chs3: "cell_AK"
+  },
+  {
+    chs4: "cell_QQ"
+  },
+  {
+    chs5: "cell_KA"
+  },
+  {
+    chs6: "cell_JJ"
+  },
+  {
+    chs7: "cell_TT"
+  },
+  {
+    chs8: "cell_AQ"
+  },
+  {
+    chs9: "cell_99"
+  },
+  {
+    chs10: "cell_QA"
+  },
+  {
+    chs11: "cell_88"
+  },
+  {
+    chs12: "cell_AJ"
+  },
+  {
+    chs13: "cell_77"
+  },
+  {
+    chs14: "cell_AT"
+  },
+  {
+    chs15: "cell_JA"
+  },
+  {
+    chs16: "cell_66"
+  },
+  {
+    chs17: "cell_KQ"
+  },
+  {
+    chs18: "cell_TA"
+  },
+  {
+    chs19: "cell_A9"
+  },
+  {
+    chs20: "cell_KJ"
+  },
+  {
+    chs21: "cell_A8"
+  },
+  {
+    chs22: "cell_55"
+  },
+  {
+    chs23: "cell_QK"
+  },
+  {
+    chs24: "cell_9A"
+  },
+  {
+    chs25: "cell_A7"
+  },
+  {
+    chs26: "cell_8A"
+  },
+  {
+    chs27: "cell_KT"
+  },
+  {
+    chs28: "cell_A6"
+  },
+  {
+    chs29: "cell_44"
+  },
+  {
+    chs30: "cell_A5"
+  },
+  {
+    chs31: "cell_7A"
+  },
+  {
+    chs32: "cell_QJ"
+  },
+  {
+    chs33: "cell_A4"
+  },
+  {
+    chs34: "cell_JK"
+  },
+  {
+    chs35: "cell_A3"
+  },
+  {
+    chs36: "cell_A2"
+  },
+  {
+    chs37: "cell_K9"
+  },
+  {
+    chs38: "cell_TK"
+  },
+  {
+    chs39: "cell_33"
+  },
+  {
+    chs40: "cell_QT"
+  },
+  {
+    chs41: "cell_5A"
+  },
+  {
+    chs42: "cell_6A"
+  },
+  {
+    chs43: "cell_JQ"
+  },
+  {
+    chs44: "cell_K8"
+  },
+  {
+    chs45: "cell_JT"
+  },
+  {
+    chs46: "cell_4A"
+  },
+  {
+    chs47: "cell_9K"
+  },
+  {
+    chs48: "cell_3A"
+  },
+  {
+    chs49: "cell_Q9"
+  },
+  {
+    chs50: "cell_2A"
+  },
+  {
+    chs51: "cell_K7"
+  },
+  {
+    chs52: "cell_TQ"
+  },
+  {
+    chs53: "cell_22"
+  },
+  {
+    chs54: "cell_K6"
+  },
+  {
+    chs55: "cell_8K"
+  },
+  {
+    chs56: "cell_Q8"
+  },
+  {
+    chs57: "cell_K5"
+  },
+  {
+    chs58: "cell_J9"
+  },
+  {
+    chs59: "cell_7K"
+  },
+  {
+    chs60: "cell_TJ"
+  },
+  {
+    chs61: "cell_9Q"
+  },
+  {
+    chs62: "cell_K4"
+  },
+  {
+    chs63: "cell_K3"
+  },
+  {
+    chs64: "cell_T9"
+  },
+  {
+    chs65: "cell_6K"
+  },
+  {
+    chs66: "cell_Q7"
+  },
+  {
+    chs67: "cell_J8"
+  },
+  {
+    chs68: "cell_K2"
+  },
+  {
+    chs69: "cell_8Q"
+  },
+  {
+    chs70: "cell_Q6"
+  },
+  {
+    chs71: "cell_5K"
+  },
+  {
+    chs72: "cell_9J"
+  },
+  {
+    chs73: "cell_T8"
+  },
+  {
+    chs74: "cell_Q5"
+  },
+  {
+    chs75: "cell_4K"
+  },
+  {
+    chs76: "cell_J7"
+  },
+  {
+    chs77: "cell_3K"
+  },
+  {
+    chs78: "cell_Q4"
+  },
+  {
+    chs79: "cell_9T"
+  },
+  {
+    chs80: "cell_98"
+  },
+  {
+    chs81: "cell_7Q"
+  },
+  {
+    chs82: "cell_Q3"
+  },
+  {
+    chs83: "cell_2K"
+  },
+  {
+    chs84: "cell_J3"
+  },
+  {
+    chs85: "cell_8J"
+  },
+  {
+    chs86: "cell_6Q"
+  },
+  {
+    chs87: "cell_T7"
+  },
+  {
+    chs88: "cell_Q2"
+  },
+  {
+    chs89: "cell_J6"
+  },
+  {
+    chs90: "cell_5Q"
+  },
+  {
+    chs91: "cell_J5"
+  },
+  {
+    chs92: "cell_97"
+  },
+  {
+    chs93: "cell_87"
+  },
+  {
+    chs94: "cell_8T"
+  },
+  {
+    chs95: "cell_7J"
+  },
+  {
+    chs96: "cell_4Q"
+  },
+  {
+    chs97: "cell_T6"
+  },
+  {
+    chs98: "cell_J4"
+  },
+  {
+    chs99: "cell_3Q"
+  },
+  {
+    chs100: "cell_89"
+  },
+  {
+    chs101: "cell_96"
+  },
+  {
+    chs102: "cell_86"
+  },
+  {
+    chs103: "cell_7T"
+  },
+  {
+    chs104: "cell_6J"
+  },
+  {
+    chs105: "cell_2Q"
+  },
+  {
+    chs106: "cell_J2"
+  },
+  {
+    chs107: "cell_T5"
+  },
+  {
+    chs108: "cell_76"
+  },
+  {
+    chs109: "cell_5J"
+  },
+  {
+    chs110: "cell_T4"
+  },
+  {
+    chs111: "cell_79"
+  },
+  {
+    chs112: "cell_4J"
+  },
+  {
+    chs113: "cell_95"
+  },
+  {
+    chs114: "cell_T3"
+  },
+  {
+    chs115: "cell_65"
+  },
+  {
+    chs116: "cell_78"
+  },
+  {
+    chs117: "cell_85"
+  },
+  {
+    chs118: "cell_75"
+  },
+  {
+    chs119: "cell_54"
+  },
+  {
+    chs120: "cell_6T"
+  },
+  {
+    chs121: "cell_3J"
+  },
+  {
+    chs122: "cell_T2"
+  },
+  {
+    chs123: "cell_94"
+  },
+  {
+    chs124: "cell_69"
+  },
+  {
+    chs125: "cell_64"
+  },
+  {
+    chs126: "cell_84"
+  },
+  {
+    chs127: "cell_74"
+  },
+  {
+    chs128: "cell_68"
+  },
+  {
+    chs129: "cell_67"
+  },
+  {
+    chs130: "cell_53"
+  },
+  {
+    chs131: "cell_2J"
+  },
+  {
+    chs132: "cell_5T"
+  },
+  {
+    chs133: "cell_4T"
+  },
+  {
+    chs134: "cell_93"
+  },
+  {
+    chs135: "cell_59"
+  },
+  {
+    chs136: "cell_92"
+  },
+  {
+    chs137: "cell_43"
+  },
+  {
+    chs138: "cell_63"
+  },
+  {
+    chs139: "cell_83"
+  },
+  {
+    chs140: "cell_56"
+  },
+  {
+    chs141: "cell_73"
+  },
+  {
+    chs142: "cell_58"
+  },
+  {
+    chs143: "cell_57"
+  },
+  {
+    chs144: "cell_82"
+  },
+  {
+    chs145: "cell_52"
+  },
+  {
+    chs146: "cell_3T"
+  },
+  {
+    chs147: "cell_2T"
+  },
+  {
+    chs148: "cell_49"
+  },
+  {
+    chs149: "cell_45"
+  },
+  {
+    chs150: "cell_42"
+  },
+  {
+    chs151: "cell_62"
+  },
+  {
+    chs152: "cell_72"
+  },
+  {
+    chs153: "cell_46"
+  },
+  {
+    chs154: "cell_32"
+  },
+  {
+    chs155: "cell_48"
+  },
+  {
+    chs156: "cell_47"
+  },
+  {
+    chs157: "cell_35"
+  },
+  {
+    chs158: "cell_34"
+  },
+  {
+    chs159: "cell_93"
+  },
+  {
+    chs160: "cell_29"
+  },
+  {
+    chs161: "cell_36"
+  },
+  {
+    chs162: "cell_38"
+  },
+  {
+    chs163: "cell_28"
+  },
+  {
+    chs164: "cell_73"
+  },
+  {
+    chs165: "cell_25"
+  },
+  {
+    chs166: "cell_42"
+  },
+  {
+    chs167: "cell_72"
+  },
+  {
+    chs168: "cell_26"
+  },
+  {
+    chs169: "cell_23"
+  }
+]
