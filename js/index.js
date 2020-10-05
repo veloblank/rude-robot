0159fc08a7e720fc471909a2eaa97207f0ded907
John Blank <veloblank@gmail.com>
2020-10-05 04:46:07 -0400

    Remove complicating div from td.

--- js/index.js
+++ js/index.js
@@ -10,15 +10,33 @@ function addTableListeners() {
   let slider = document.getElementById("range-slider");
   for (let table of tables) {
     table.addEventListener("mouseenter", (e) => {
-      let rangeValue = (parseInt(e.target.getAttribute("data-range")))
-      slider.innerHTML = `<span id="range-value">${rangeValue}%</span>`;
+      table.onwheel = scrollNumbers;
     })
   }
 }
 
 function addSliderListeners() {
   let slider = document.getElementById("range-slider");
-  slider.addEventListener("mouseenter", (e) => {
-    console.log(e.target)
-  })
+}
+
+function scrollNumbers(e) {
+  e.preventDefault()
+  let target = e.target
+  if (!!target.classList && target.classList[0].includes("grid")) {
+    let targetDataRange = parseInt(target.getAttribute("data-range"))
+    let newDivValue;
+    if (e.deltaY < 0) {
+      newDivValue = targetDataRange + 1
+      target.setAttribute("data-range", newDivValue)
+      console.log(target.getAttribute("data-range"))
+      slider.innerHTML = `<span id="range-value">${rangeInt}%</span>`;
+    } else {
+      newDivValue = targetDataRange - 1
+      target.setAttribute("data-range", newDivValue)
+      slider.innerHTML = `<span id="range-value">${rangeInt}%</span>`;
+    }
+    console.log(target)
+  } else {
+    console.log(target)
+  }
 }
