cb9ef9e186526e12b843da6cbceee950b5cec9bd
John Blank <veloblank@gmail.com>
2020-10-06 10:36:16 -0400

    Redo JS functions. Separate listeners. The current JS functions are not getting the correct event target for onwheel. Added listeners that correctly identified the element (mouseenter) and bound it to onwheel event immediately.

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,25 @@
 import { BLOCH_CALL } from "./blochCallArray.js"
-window.onload = addTableListeners();
+import RangeCounter from "./RangeCounter.js"
+window.onload = addTableListener();
 
-function addTableListeners() {
-  let tables = document.querySelectorAll(".grid");
-  for (let table of tables) {
-    table.addEventListener("mouseenter", (e) => {
-      findEmptyTableTds(e);
-    });
+function addTableListener() {
+  let grids = document.querySelectorAll(".grid");
+  for (let grid of grids) {
+    grid.addEventListener("mouseenter", e => {
+      let target = e.target;
+      addWheelListener(target);
+    })
   }
 }
 
-function findEmptyTableTds(e) {
-  let gridTarget = e.target;
-  let tableId = e.target.getAttribute("id");
-  let gridValue = parseInt(gridTarget.getAttribute("data-range"));
-  let rangeArray = BLOCH_CALL.splice(0, gridValue)
-  gridTarget.onwheel = (e) => {
-    handleScroll(e, rangeArray, tableId, gridValue, gridTarget)
-  };
-}
-
-function handleScroll(e, rangeArray, tableId, gridValue, gridTarget) {
-  e.preventDefault();
-  if (e.deltaY < 0 && gridValue < 100) {
-    gridValue += 1
-    gridTarget.setAttribute("data-range", gridValue)
-    addClassesToTds(e, rangeArray, tableId)
-  } else if (e.deltaY > 0 && gridValue > 0) {
-    gridValue -= 1
-    gridTarget.setAttribute("data-range", gridValue)
-    addClassesToTds(e, rangeArray, tableId)
-  } else {
-    console.log("Nothing!")
-  }
-}
-
-function addClassesToTds(e, rangeArray, table) {
-  let tds = document.querySelectorAll(`#${table} td`);
-  for (let td of tds) {
-    if (rangeArray.includes(`${td.getAttribute("id")}`)) {
-      document.querySelector(`#${td.getAttribute("id")}`).classList.add("highlight")
+function addWheelListener(target) {
+  target.onwheel = (e) => {
+    e.preventDefault();
+    let targetRange = target.getAttribute("data-range");
+    if (e.deltaY > 0) {
+      console.log("Scrolling up!")
+    } else if (e.deltaY < 0) {
+      console.log("Scrolling down!")
     }
   }
 }
-
