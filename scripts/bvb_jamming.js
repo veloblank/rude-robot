5bb7213be6352cececabbe0e567e5a7d30947591
John Blank <veloblank@gmail.com>
2020-10-15 17:05:23 -0400

    Add BvB hand rankings.

---
+++ scripts/bvb_jamming.js
@@ -0,0 +1,202 @@
+let bvbJamming = [
+  {
+    10: [
+      "cell_AA",
+      "cell_KK",
+      "cell_QQ",
+      "cell_JJ",
+      "cell_TT",
+      "cell_99",
+      "cell_88",
+      "cell_AK",
+      "cell_77",
+      "cell_AQ",
+      "cell_KA"
+    ]
+  }, {
+    9: [
+      "cell_AJ",
+      "cell_AT",
+      "cell_QA",
+      "cell_JA",
+      "cell_KQ",
+      "cell_66",
+      "cell_A9",
+      "cell_TA",
+      "cell_KJ",
+      "cell_A8",
+      "cell_KT"
+    ]
+  }, {
+    8: [
+      "cell_QK",
+      "cell_A7",
+      "cell_9A",
+      "cell_JK",
+      "cell_55",
+      "cell_QJ",
+      "cell_K9",
+      "cell_A5",
+      "cell_A6",
+      "cell_8A",
+      "cell_TK",
+      "cell_QT",
+      "cell_A4",
+      "cell_7A",
+      "cell_K8",
+      "cell_A3",
+      "cell_5A",
+      "cell_6A",
+      "cell_K7",
+      "cell_JT",
+      "cell_A2"
+    ]
+  }, {
+    7: [
+      "cell_JQ",
+      "cell_9K",
+      "cell_Q9",
+      "cell_TQ",
+      "cell_44",
+      "cell_4A",
+      "cell_K6",
+      "cell_8K",
+      "cell_Q8",
+      "cell_3A",
+      "cell_K5",
+      "cell_J9"
+    ]
+  }, {
+    6: [
+      "cell_9Q",
+      "cell_TJ",
+      "cell_7K",
+      "cell_2A",
+      "cell_K4",
+      "cell_Q7",
+      "cell_6K",
+      "cell_K3",
+      "cell_T9",
+      "cell_J8",
+      "cell_33",
+      "cell_Q6",
+      "cell_8Q",
+      "cell_5K",
+      "cell_9J",
+      "cell_K2",
+      "cell_Q5",
+      "cell_T8",
+      "cell_4K",
+      "cell_J7",
+      "cell_Q4",
+      "cell_7Q",
+      "cell_9T",
+      "cell_8J",
+      "cell_3K",
+      "cell_6Q",
+      "cell_Q3",
+      "cell_98",
+      "cell_T7",
+      "cell_J6",
+      "cell_2K",
+      "cell_22"
+    ]
+  }, {
+    5: [
+      "cell_Q2",
+      "cell_5Q",
+      "cell_J5",
+      "cell_8T",
+      "cell_7J",
+      "cell_4Q",
+      "cell_97",
+      "cell_J4",
+      "cell_T6",
+      "cell_J3",
+      "cell_3Q",
+      "cell_89",
+      "cell_87"
+    ]
+  }, {
+    4: [
+      "cell_7T",
+      "cell_6J",
+      "cell_96",
+      "cell_J2",
+      "cell_2Q",
+      "cell_T5",
+      "cell_5J",
+      "cell_T4",
+      "cell_79",
+      "cell_86",
+      "cell_4J",
+      "cell_6T",
+      "cell_95",
+      "cell_T3",
+      "cell_76",
+      "cell_3J",
+      "cell_78",
+      "cell_T2",
+      "cell_85",
+      "cell_69",
+      "cell_2J",
+      "cell_5T",
+      "cell_94"
+    ]
+  }, {
+    3: [
+      "cell_75",
+      "cell_4T",
+      "cell_93",
+      "cell_68",
+      "cell_65",
+      "cell_84",
+      "cell_59",
+      "cell_3T",
+      "cell_92",
+      "cell_67",
+      "cell_74",
+      "cell_2T",
+      "cell_54",
+      "cell_58",
+      "cell_64",
+      "cell_83",
+      "cell_49"
+    ]
+  }, {
+    2: [
+      "cell_57",
+      "cell_82",
+      "cell_73",
+      "cell_39",
+      "cell_56",
+      "cell_63",
+      "cell_48",
+      "cell_43",
+      "cell_53"
+    ]
+  }, {
+    1: [
+      "cell_29",
+      "cell_47",
+      "cell_72",
+      "cell_45",
+      "cell_46",
+      "cell_52",
+      "cell_62",
+      "cell_38",
+      "cell_42",
+      "cell_28",
+      "cell_37",
+      "cell_35",
+      "cell_36",
+      "cell_32",
+      "cell_34",
+      "cell_27",
+      "cell_25",
+      "cell_26",
+      "cell_24",
+      "cell_23"
+    ]
+  }
+]
